717f41f82fe2acb182a682caa30733bc4ae84f5e
Not-LastTry <artyomkan2605@gmail.com>
2025-03-09 17:49:26 +0300

    Comm 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+namespace BackEnd_Нагорнов_А.В._ЛР2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+            app.Run();
+        }
+    }
+}
